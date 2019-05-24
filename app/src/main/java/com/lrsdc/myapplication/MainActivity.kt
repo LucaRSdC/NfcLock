a6e4dee7fc7d357c61ae0b10b75508877567508e
spilu <spilucco90@gmail.com>
2019-05-24 17:48:41 +0200

    Initial commit

---
+++ app/src/main/java/com/lrsdc/myapplication/MainActivity.kt
@@ -0,0 +1,136 @@
+package com.lrsdc.myapplication
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.ActionBar
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.*
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
+    private val  RC_SIGN_IN = 101
+    lateinit var toolBar : ActionBar
+
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        FirebaseApp.initializeApp(this);
+
+
+        mAuth = FirebaseAuth.getInstance(); //firebase
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        //bottom menù config
+        toolBar = supportActionBar!!
+        val bottomNavigation: BottomNavigationView = bottom_nav
+        val navController = findNavController(this@MainActivity, R.id.fragment_nav)
+
+        setupWithNavController(this.bottom_nav, navController)
+        setupActionBarWithNavController(this@MainActivity,navController)
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth.currentUser
+        signIn()
+        updateUI(currentUser)
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.pulsanteSerratura -> {
+                Navigation.findNavController(this,R.id.fragment_nav).navigate(R.id.pulsanteSerratura)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.frameCarte -> {
+                Navigation.findNavController(this,R.id.fragment_nav).navigate(R.id.frameCarte)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.frameNFC -> {
+                Navigation.findNavController(this,R.id.fragment_nav).navigate(R.id.frameNFC)
+
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    fun signIn() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("MyActivity", "Google sign in failed", e)
+                // ...
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.d("MyActivity", "firebaseAuthWithGoogle:" + acct.id!!)
+
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("MyActivity", "signInWithCredential:success")
+                    val user = mAuth.currentUser
+                    Toast.makeText(applicationContext, "Connesso", Toast.LENGTH_SHORT)
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("MyActivity", "signInWithCredential:failure", task.exception)
+                    Toast.makeText(applicationContext, "Connessione Fallita", Toast.LENGTH_SHORT)
+                    //Snackbar.make(applicationContext, "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+
+                // ...
+            }
+    }
+
+    private fun updateUI(U : FirebaseUser?){
+    //non fa ancora niente
+        //
+    }
+}
---
+++ app/src/main/java/com/lrsdc/myapplication/PulsanteSerratura.kt
@@ -0,0 +1,83 @@
+package com.lrsdc.myapplication
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_carte.*
+import kotlinx.android.synthetic.main.fragment_nfc.*
+import kotlinx.android.synthetic.main.fragment_pulsante_serratura.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class PulsanteSerratura : Fragment() {
+
+    private lateinit var database: DatabaseReference //lateinit serve per non inizializzare la variabile
+    private lateinit var value: String
+    private lateinit var ref : DatabaseReference
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        //
+        FirebaseApp.initializeApp(this.context);
+        database = FirebaseDatabase.getInstance().reference //oggetto di tipo riferimento che ci permette di recuperare valori sul database
+        val nodo = "LED_STATUS"
+        ref = database.child(nodo) //riferimento al elemento da noi cercato
+        ref.addValueEventListener(ReadData)//stiamo configurando il listener per sapere se il valore di cui abbiamo il riferimento viene modificato
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pulsante_serratura, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        this.button.setOnClickListener {
+            //qui si chiama la funzione per leggere
+            if (value == "OFF") {
+                ref.setValue("ON")//modifica valore presente sul DataBase
+                Log.d("MyActivity", "L'ho Acceso")
+            } else if (value == "ON") {
+                ref.setValue("OFF")
+                Log.d("MyActivity", "L'ho spento")
+            }
+        }
+    }
+
+    var ReadData = object : ValueEventListener {
+        override fun onDataChange(dataSnapshot: DataSnapshot) {
+            // This method is called once with the initial value and again
+            // whenever data at this location is updated.
+            value = dataSnapshot.getValue(String::class.java)!!
+            Log.d("MyActivity", "Value is: " + value)
+            if(value =="OFF"){
+                ledStatus?.text = resources.getString(R.string.OFF_String) //il punto interrogativo è necessario anche se è sempre non null, altrimenti crasha
+            }else{
+                ledStatus?.text = resources.getString(R.string.On_string)
+            }
+
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            // Failed to read value
+            Log.w("MyActivity", "Failed to read value.", error.toException())
+        }
+    }
+
+
+}
